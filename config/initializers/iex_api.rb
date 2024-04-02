6a05566c7c31faa6afeafb941f0bc59e058b1b86
p3rc1us <kervymaniego@gmail.com>
2024-04-02 09:15:41 +0800

    secret on

--- config/initializers/iex_api.rb
+++ config/initializers/iex_api.rb
@@ -2,6 +2,6 @@ require 'iex-ruby-client'
 
 IEX::Api.configure do |config|
   config.publishable_token = 'pk_c66a213353324c4aba06a16573872c92' # defaults to ENV['IEX_API_PUBLISHABLE_TOKEN']
-  # config.secret_token = 'sk_07f6a9afe36c4b16b0ef09eff899f93b' # defaults to ENV['IEX_API_SECRET_TOKEN']
+  config.secret_token = 'sk_07f6a9afe36c4b16b0ef09eff899f93b' # defaults to ENV['IEX_API_SECRET_TOKEN']
   config.endpoint = 'https://cloud.iexapis.com/v1' # use 'https://sandbox.iexapis.com/v1' for Sandbox
 end
